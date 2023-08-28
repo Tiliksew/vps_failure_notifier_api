609e232eb205d23d7b1064739a8f5765de94329f
Tiliksew Mulugeta <tiliksewmulugeta552@gmail.com>
2023-08-28 11:24:10 +0300

    initial commit

---
+++ helpers.js
@@ -0,0 +1,57 @@
+exports.sendSms = async (phoneNumber, message, next) => {
+	const intlPhone =  ((phoneNumber.startsWith('251')) || (phoneNumber.startsWith('251')) )?`${phoneNumber}`:phoneNumber.slice(-12);	
+	const username = (phoneNumber.startsWith('251') || phoneNumber.startsWith('+251'))?`mamapays`:`mobifins`;
+    const requestBod = {
+        "message": message,
+        "mobile": intlPhone
+    }
+	console.log(requestBod)
+    try {
+        const auth = Buffer.from(`${username}:KmTt5#D_`).toString('base64');
+        const response = await fetch(
+            'https://helloomarket.com/api/send_mp_sms.php', {
+            method: 'POST',
+            body: JSON.stringify(requestBod),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${auth}`
+            }
+        }
+        );
+
+        const value = await response.json();
+	    console.log(value)
+        console.log({ message: value['output'], level: value['op_status'] == 'successful' ? 'info' : 'error' });
+        
+    } catch (e) {
+        // next(e);
+        return res.json({error:'Server Error' })
+    }
+}
+
+exports.callAnEndPointToVps = async () => {
+    try {
+	console.log('Hello')
+
+        const response = await fetch(
+            'https://bctsdev.giize.com/etswitch-banks/banks', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        );
+
+        // const value = await response.json();
+	console.log('inside method',response)
+    if(response.status>=500){
+    return false;        
+    }
+    return true;        
+    } catch (e) {
+	console.log('inside error',e)
+    return false;        
+        
+        // next(e);
+    }
+}
---
+++ index.js
@@ -0,0 +1,57 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+
+
+const port = process.env.PORT || 5001;
+const helpers = require("./helpers");
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.use((req, res, next) => {
+    next();
+});
+
+app.use('/welcome', (req, res) => {
+    res.status(200).json({ message: 'Welcome' });
+});
+
+app.use('/call', async (req, res,next) => {
+
+    let testResult = await helpers.callAnEndPointToVps()
+    console.log('the vps result is ',testResult);
+    if(testResult){
+        res.status(200).json({ message: 'No Errors Found Mr,Tilik' });
+
+    }
+    else{
+
+        
+            let currentDate = new Date();
+            let theTime = currentDate.toLocaleTimeString();
+            let theDate =currentDate.toDateString();
+        
+            let smsMessageBody = `Hey Dev Tilik, The VPS stops working.\nDate: ${theDate}\nTime: ${theTime}`;
+            await helpers.sendSms('251919298457',smsMessageBody,next);
+            res.status(200).json({ message: 'Succesfully Notified Mr,Tiliksew' });
+    }
+    
+});
+
+// app.use('*', endpointNotFoundError);
+// app.use(errorHandler);
+server.listen(port, () => {
+    console.log({ message: `Server running on port ${port}`, level: 'info' });
+});
+
+module.exports = app;
